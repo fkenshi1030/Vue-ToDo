3b2c42cdbcabe2f5f902592208e6f01f60415e36
藤井けんし <f.kenshi@fujiikenshinoMacBook-puro.local>
2020-04-06 21:49:12 +0900

    main.js完成(Delete)は後で追加する

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 var STORAGE_KEY = 'todos-vuejs-demo'
 var todoStorage = {
   fetch: function() {
-    var todos = JSON.parse(
-      localStorage.getItem(STORAGE_KEY) || '[]'
-    )
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
     todos.forEach(function(todo, index) {
       todo.id = index
     })
@@ -14,6 +12,7 @@ var todoStorage = {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
   }
 }
+
 //コンストラクタ関数Vueを使ってルートインスタンス作成
 //アプリケーションで使用したいデータはdataオプションへ登録
 new Vue ({
@@ -40,39 +39,65 @@ new Vue ({
         return this.todos.filter(function(el) {
           return this.current < 0 ? true : this.current === el.state
         }, this)
+      },
+
+       // ★STEP13 作業中・完了のラベルを表示する
+       labels() {
+        return this.options.reduce(function (a, b) {
+          return Object.assign(a, { [b.value]: b.label })
+        }, {})
+        // キーから見つけやすいように、次のように加工したデータを作成
+        // {0: '作業中', 1: '完了', -1: 'すべて'}
       }
     },
 
-    methods: {
-      //ToDo 追加の管理
+  // ★STEP8
+  watch: {
+    // オプションを使う場合はオブジェクト形式にする
+    todos: {
+      // 引数はウォッチしているプロパティの変更後の値
+      handler: function (todos) {
+        todoStorage.save(todos)
+      },
+      // deep オプションでネストしているデータも監視できる
+      deep: true
+    }
+  },
+
+  // ★STEP9
+  created() {
+    // インスタンス作成時に自動的に fetch() する
+    this.todos = todoStorage.fetch()
+  },
+
+  methods: {
     doAdd: function(event, value) {
-        //ref で名前をつけておいた要素を参照
+      // ref で名前を付けておいた要素を参照
       var comment = this.$refs.comment
-      //入力がなければ何もしないで return
+      // 入力がなければ何もしないで return
       if (!comment.value.length) {
         return
       }
-      // {新しいID, コメント, 作業状態}
-      // というオブジェクトを現在のtodosリストへpush
+      // { 新しいID, コメント, 作業状態 }
+      // というオブジェクトを現在の todos リストへ push
       // 作業状態「state」はデフォルト「作業中=0」で作成
       this.todos.push({
         id: todoStorage.uid++,
         comment: comment.value,
         state: 0
       })
-      //フォーム要素を空にする
+      // フォーム要素を空にする
       comment.value = ''
     },
-    watch: {
-      todos: {
-        handler: function(todos) {
-          todoStorage.save(todos)
-        },
-        deep: true
-      }
+
+    doChangeState: function (item) {
+      item.state = !item.state ? 1 : 0
     },
-    created() {
-      this.todos = todoStorage.fetch()
+
+    doRemove: function (item) {
+      var index = this.todos.indexOf(item)
+      this.todos.splice(index, 1)
     }
+
   }
 })
